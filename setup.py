639b0543264a9e723a0691aece845fd66dd12fad
Naveen <naveenhn.sap@gmail.com>
2023-09-07 16:19:10 +0530

    Requirements Added2

--- setup.py
+++ setup.py
@@ -6,10 +6,10 @@ with open("README.md", "r", encoding="utf-8") as f:
 
 __version__ = "0.0.0"
 
-REPO_NAME = "End-to-end-ML-Project-with-MLflow"
-AUTHOR_USER_NAME = "entbappy"
+REPO_NAME = "ML_E2E"
+AUTHOR_USER_NAME = "NAveen"
 SRC_REPO = "mlProject"
-AUTHOR_EMAIL = "entbappy73@gmail.com"
+AUTHOR_EMAIL = "Naveenhn.sap@gmail.com"
 
 
 setuptools.setup(
